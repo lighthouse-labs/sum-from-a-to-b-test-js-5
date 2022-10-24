32acffd20f453b69e4febc795475f243f99fea31
babu0126 <18davidwang@gmail.com>
2022-10-23 17:43:33 -0700

    Recurseion of summing between two numbers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) return toN;
+  if (fromN < toN )
+  return fromN + sum(fromN + 1, toN);
 }
+console.log(sum(1,4));
 
 module.exports = sum;
